7e0efd15deb7b99324ae3a6dd0cc2f89f91b225d
Gautam Prakash <gautamp@reapbenefit.org>
2018-01-19 16:33:05 +0530

    PIR Sensor and Index.html

--- Sensors Overview/PIR Sensor/Uno_PIR/Uno_PIR.ino
+++ Sensors Overview/PIR Sensor/Uno_PIR/Uno_PIR.ino
@@ -1,37 +1,38 @@
 /*
- * PIR sensor tester
- * Read More here: https://learn.adafruit.com/pir-passive-infrared-proximity-motion-sensor?view=all
- * Thanks Adafruit!
- */
- 
+   PIR sensor tester
+   Read More here: https://learn.adafruit.com/pir-passive-infrared-proximity-motion-sensor?view=all
+   Thanks Adafruit!
+*/
+
 int ledPin = 13;                // choose the pin for the LED
 int inputPin = 2;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
 int count = 0;
- 
+
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
- 
+
   Serial.begin(9600);
 }
- 
-void loop(){
+
+void loop() {
   val = digitalRead(inputPin);  // read input value
+  count = count + 1;
+  Serial.println(count);
   if (val == HIGH) {            // check if the input is HIGH
     digitalWrite(ledPin, HIGH);  // turn LED ON
     if (pirState == LOW) {
       // we have just turned on
       Serial.println("Motion detected!");
-      count = count + 1;
-      Serial.println(count);
+
       // We only want to print on the output change, not state
       pirState = HIGH;
     }
   } else {
     digitalWrite(ledPin, LOW); // turn LED OFF
-    if (pirState == HIGH){
+    if (pirState == HIGH) {
       // we have just turned of
       Serial.println("Motion ended!");
       count = 0;
