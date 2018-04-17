82dbb541027368aac61893b4143af274b3fcba91
gtmprkshrb <gautamp@reapbenefit.org>
2018-04-17 11:24:00 +0530

    updated dry waste bin (buzzer)

--- Waste/Dry Waste Bin(buzzer)/code001/code001.ino
+++ Waste/Dry Waste Bin(buzzer)/code001/code001.ino
@@ -1,30 +1,16 @@
-//soil moisture sensor
-#define soilMoisture A0 //connect analog pin of soil moisture sensor to A0 of Arduino                   
-//calibration
-//maximum value detected(dry) 1025
-//minimum value detected(wet) 10
-//percentage of moisture= 100-(detected/maximum*100)
-//100/maximum=100/1025=0.0975
+#define soilMoisture A1 //connect analog pin of soil moisture sensor to A0 of Arduino                   
 
 unsigned long moisture;
 float percentage;
 
 void setup() {
-  // put your setup code here, to run once:
-   // Initialize a serial connection for reporting values to the host
   Serial.begin(115200);
   pinMode(soilMoisture,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
    moisture=analogRead(soilMoisture);//read input from pin A0(soilMoisture)
    Serial.print("Moisture: ");
    Serial.print(moisture);
-   Serial.print("\t");
-   percentage=100-(moisture*0.0975);//express as a percentage
-   Serial.print("Percentage: ");
-   Serial.print(percentage);
-   Serial.println("%");
    delay(3000);
 }
