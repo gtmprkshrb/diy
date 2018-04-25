0a53e8b5414386c7f30a9ce6870044b701a0fde1
GautamP <gautamp@GautamPs-MacBook-Pro.local>
2018-04-25 17:03:16 +0530

    diy>other>vandalism code changed

--- Other/Vandalism Detector(Buzzer)/Code001/Code001.ino
+++ Other/Vandalism Detector(Buzzer)/Code001/Code001.ino
@@ -1,34 +1,29 @@
-#define trigPin 10
-#define echoPin 9
-int buzzer = 13;
+#define trigPin 12
+#define echoPin 11
 
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
-  delay(100);
   int timetaken, dist;
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(1000);
   digitalWrite(trigPin, LOW);
   timetaken = pulseIn(echoPin, HIGH);
   dist = (timetaken / 2) * 0.034049 ;
-  Serial.print("Distance in CM:");
-  Serial.println(dist);
-  if (dist > 0 && dist < 46) {
-
-    tone(buzzer, 1000, 500);
-    delay(500);
+  if (dist >= 300 || dist <= 2) //CHANGE
+  {
+    Serial.print("\n Out Of Range");
   }
-  else  {
-    digitalWrite(buzzer, LOW);
-   
+  else {
+
+    Serial.println("\n Distance in CM: ");
+
+    Serial.print( dist);
 
   }
-  delay(500);
+  delay(1000);
 }
-
