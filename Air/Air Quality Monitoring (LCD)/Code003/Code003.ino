81bae1853b4d8e5bcffe4c22b551739c82f88c34
Sairam <silentsairam@gmail.com>
2018-10-10 17:03:02 +0530

    Update Code003.ino

--- Air/Air Quality Monitoring (LCD)/Code003/Code003.ino
+++ Air/Air Quality Monitoring (LCD)/Code003/Code003.ino
@@ -1,48 +1,99 @@
-//This code explains how to periodically note and display humidity and temperature
-//values in LCD with DHT11 sensor
+//This is the combined code of DHT sensor & Sharp dust sensor and values will be displayed on LCD screen
+//Data can be seen live on Dashboard
 
-#include <Wire.h>
-#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino 
+#include <PubSubClient.h>
+#include<Wire.h>
 
-#include <LiquidCrystal_PCF8574.h>
-LiquidCrystal_PCF8574 lcd(0x3F); //change the address if necessary
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include "DHT.h"
+#include <LiquidCrystal_I2C.h>
 
-int measurePin = A5;
-int ledPower = 12;
-int dhtpin = 2;
-#define DHTTYPE DHT11
+//DHT Section setup
+#define DHTPIN D6
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+float h, t;
+#define WLAN_SSID  "//*your ssid here" // The SSID of your local LAN where a MQTT-broker is running
+#define WLAN_PASS  "//password here" // The WIFI password for your local lan
+#define BROKER_IP  "13.71.3.239" // The IP of the machine where the broker is running
+#define BROKER_PORT 1883
+#define CLIENT_NAME "Air"
+WiFiClient wifiClient;
+PubSubClient mqttClient(BROKER_IP, BROKER_PORT, wifiClient);
 
-unsigned int samplingTime = 280;
-unsigned int deltaTime = 40;
-unsigned int sleepTime = 9680;
+//LCD
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // check the lcd i2c number
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+//Sharp Dust Sensor
+int measurePin = A0;
+int ledPower = D0;
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
 float voMeasured = 0;
 float calcVoltage = 0;
 float dustDensity = 0;
-
-
-DHT dht (2, DHT11);
+const int numReadings = 5;
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;
 
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  Wire.begin();
-  pinMode(ledPower, OUTPUT);
-  pinMode(dhtpin, OUTPUT);
-  delay(1000);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
   dht.begin();
-  //lcd.home();   // initializing the LCD
-  lcd.begin(16, 2); // initialize the lcd
-  lcd.setBacklight(200);
-  lcd.home(); lcd.clear();
-  lcd.print("Hello LCD");
-  delay(1000);
-
+  lcd.init();   // initializing the LCD
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Setting Up");
+  lcd.clear();
+  pinMode(ledPower, OUTPUT);
+  pinMode(measurePin, OUTPUT);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  dustdata();
+  temp();
+  showonlcd();
+  senddata();
   delay(15000);
-  digitalWrite(dhtpin, HIGH);
+  lcd.clear();
+}
+
+//Functions written now on
+
+void temp() {
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.println();
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+}
+
+void dustdata() {
+
+
   digitalWrite(ledPower, LOW); // power on the LED
   delayMicroseconds(samplingTime);
 
@@ -52,52 +103,125 @@ void loop() {
   digitalWrite(ledPower, HIGH); // turn the LED off
   delayMicroseconds(sleepTime);
 
-  calcVoltage = voMeasured * (5.0 / 1024);
+  // 0 - 5.0V mapped to 0 - 1023 integer values
+  calcVoltage = voMeasured * (3.3 / 1024);
 
   // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
   // Chris Nafis (c) 2012
-  //  dustDensity = (0.17 * calcVoltage - 0.1) * 1000;
-  dustDensity = (0.17 * calcVoltage) * 1000;
+  dustDensity = (0.17 * calcVoltage - 0.1) * 1000;
 
-  Serial.print("Raw: ");
+  Serial.print("Raw Signal Value (0-1023): ");
   Serial.print(voMeasured);
 
-  Serial.print("; - Voltage: ");
+  Serial.print(" - Voltage: ");
   Serial.print(calcVoltage);
 
-  Serial.print("; - Dust Density [ug/m3]: ");
-  Serial.print(dustDensity);
-  Serial.println(";");
+  Serial.print(" - Dust Density: ");
+  Serial.println(dustDensity);
 
-  delay(1000);
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  total = total - readings[readIndex];
+  // read from the sensor:
+  //readings[readIndex] = analogRead(inputPin);
+  readings[readIndex] = dustDensity;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
 
-  lcd.setCursor(0, 0);
-  
-  lcd.print("T: ");
-  lcd.print(t);
-  lcd.print("H: ");
-  lcd.print(h);
-  
-  lcd.setCursor(0, 1);
-  lcd.print("Dust PM2.5: ");
-  lcd.print(dustDensity);
-  //lcd.print(" mg/m3");
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    //return;
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+
+  if (average < 0) {
+    average = -0.833 * average ;
+    Serial.println(average);
+  }
+  else {
+    Serial.println(average);
   }
 }
 
+void senddata() {
+  if (!mqttClient.connected())
+  {
+    connectToWiFi();
+    connectToBroker();
+  }
+  mqttClient.loop();
+  publishFloatValue(t, "RB/AQM/Breathe/001/Temp");
+  publishFloatValue(h, "RB/AQM/Breathe/002/Hum");
+  publishFloatValue(average, "RB/AQM/Breathe/003/Dust");
+}
 
+void connectToWiFi()
+{
+  Serial.print("Connecting to WIFI");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to WIFI!");
+}
+void connectToBroker() {
 
+  Serial.println("Connecting to broker");
+  while (!mqttClient.connect(CLIENT_NAME))
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to broker!");
+}
 
+char msg[50];
+void publishFloatValue(float value, char* topic)
+{
+  if (isnan(value))
+  {
+    Serial.println("Invalid value!");
+    return;
+  }
 
+  Serial.println("Publishing a new value");
+  ftoa(msg, value);
+  Serial.println(msg);
+  mqttClient.publish(topic, msg);
+}
 
+char *ftoa(char *buffer, float f)
+{
+  char *returnString = buffer;
+  long integerPart = (long)f;
+  itoa(integerPart, buffer, 10);
+  while (*buffer != '\0') buffer++;
+  *buffer++ = '.';
+  long decimalPart = abs((long)((f - integerPart) * 100));
+  itoa(decimalPart, buffer, 10);
+  return returnString;
+}
 
+void showonlcd() {
+  lcd.setCursor(0, 0);
+  lcd.print("T:");
+  lcd.print(t);
+  lcd.print("C H ");
+  lcd.print(h);
+  lcd.setCursor(0, 1);
+  lcd.print("PM2.5: ");
+  lcd.print(average);
 
-
-
-
+  if (average > 1 && average  <= 65) {
+    lcd.print (" GOOD ");
+  }
+  else if (average >= 66) {
+    lcd.print (" BAD ");
+  }
+}
